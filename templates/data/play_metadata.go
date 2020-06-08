851c69cb03964c77c3d1cf8d1024577828268c51
Eric Ren <eric@renf.ca>
2020-06-07 19:44:14 -0700

    update play meta data

--- templates/data/play_metadata.go
+++ templates/data/play_metadata.go
@@ -2,42 +2,33 @@ package data
 
 // Cadence requires a mapping of string->string, which can be handled through json tags when marshalling.
 // It also does not allow for null values, so we will be omitting them if empty
+// Reference: https://docs.google.com/spreadsheets/d/1muUZowii0pqoyi6OPK1VPNJi7keSc8_5zI9vu_QvfOY/edit#gid=375111836
 type PlayMetadata struct {
-	// VideoURLs  VideoUrls `sql:"video_urls"`
-	// Images     Images    `sql:"images"`
-
-	// Sport Radar Stats
-	PlayerID                  string `json:",omitempty"`
-	FullName                  string `json:",omitempty"`
-	JerseyNumber              string `json:",omitempty"`
-	TeamAtMoment              string `json:",omitempty"`
-	PrimaryPosition           string `json:",omitempty"`
-	TotalYearsExperience      string `json:",omitempty"`
-	AwayTeamScore             *int32 `json:",omitempty,string"`
-	AwayTeamName              string `json:",omitempty"`
-	HomeTeamName              string `json:",omitempty"`
-	HomeTeamScore             *int32 `json:",omitempty,string"`
-	DateOfMoment              string `json:",omitempty"`
-	TeamAtMomentNBAID         string `json:",omitempty"`
-	CurrentTeam               string `json:",omitempty"`
-	CurrentTeamID             string `json:",omitempty"`
-	Height                    *int32 `json:",omitempty,string"`
-	Weight                    *int32 `json:",omitempty,string"`
-	PlayerGameScores          string `json:",omitempty"`
-	PlayerSeasonAverageScores string `json:",omitempty"`
-	HomeTeamNbaID             string `json:",omitempty"`
-	AwayTeamNbaID             string `json:",omitempty"`
-	NbaSeason                 string `json:",omitempty"`
-	DraftYear                 *int32 `json:",omitempty,string"`
-	DraftSelection            string `json:",omitempty"`
-	DraftRound                string `json:",omitempty"`
-	Birthplace                string `json:",omitempty"`
-	Birthdate                 string `json:",omitempty"`
-	DraftTeam                 string `json:",omitempty"`
-	DraftTeamNbaID            string `json:",omitempty"`
-	PlayType                  string `json:",omitempty"`
-	PlayCategory              string `json:",omitempty"`
-	Quarter                   string `json:",omitempty"`
-	HomeTeamScoresByQuarter   string `json:",omitempty"`
-	AwayTeamScoresByQuarter   string `json:",omitempty"`
+	FullName             string
+	FirstName            string
+	LastName             string
+	Birthdate            string
+	Birthplace           string
+	JerseyNumber         string
+	DraftTeam            string `json:",omitempty"`        // Not all plays have draft information. Can be blank
+	DraftYear            *int32 `json:",omitempty,string"` // Not all plays have draft information. Can be blank
+	DraftSelection       string `json:",omitempty"`        // Not all plays have draft information. Can be blank
+	DraftRound           string `json:",omitempty"`        // Not all plays have draft information. Can be blank
+	TeamAtMomentNBAID    string
+	CurrentTeamID        string
+	TeamAtMoment         string
+	CurrentTeam          string
+	PrimaryPosition      string
+	PlayerPosition       string
+	Height               *int32 `json:",string"`
+	Weight               *int32 `json:",string"`
+	TotalYearsExperience string
+	NbaSeason            string
+	DateOfMoment         string
+	PlayCategory         string
+	PlayType             string
+	HomeTeamName         string
+	AwayTeamName         string
+	HomeTeamScore        *int32 `json:",string"`
+	AwayTeamScore        *int32 `json:",string"`
 }
