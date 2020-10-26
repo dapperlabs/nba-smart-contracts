1988ca09ff6af6fc0ff558262a635ac9080f4db0
joshuahannan <joshua.hannan@dapperlabs.com>
2020-10-26 10:38:16 -0500

    add upgrade sale templates

--- lib/go/templates/marketV2_templates.go
+++ lib/go/templates/marketV2_templates.go
@@ -138,6 +138,49 @@ func GenerateChangePriceV2Script(topshotAddr, marketAddr flow.Address, id, price
 	return []byte(fmt.Sprintf(template, topshotAddr, marketAddr, id, price))
 }
 
+func GenerateUpgradeSaleV2Script(topshotAddr, marketV1Addr, marketV2Addr flow.Address, id, price int) []byte {
+	template := `
+		import TopShot from 0x%[1]s
+		import Market from 0x%[2]s
+		import TopShotMarketV2 from 0x%[3]s
+
+		// This transaction is for a user to change a moment sale from
+		// the first version of the market contract to the second version
+
+		// Parameters
+		//
+		// tokenID: the ID of the moment whose sale is to be upgraded
+
+		transaction(tokenID: UInt64, price: UFix64) {
+
+			prepare(acct: AuthAccount) {
+
+				// Borrow a reference to the NFT collection in the signers account	
+				let nftCollection = acct.borrow<&TopShot.Collection>(from: /storage/MomentCollection)	
+					?? panic("Could not borrow from MomentCollection in storage")	
+
+				// borrow a reference to the owner's sale collection
+				let topshotSaleCollection = acct.borrow<&Market.SaleCollection>(from: /storage/topshotSaleCollection)
+					?? panic("Could not borrow from sale in storage")
+
+				let topshotSaleV2Collection = acct.borrow<&TopShotMarketV2.SaleCollection>(from: TopShotMarketV2.marketStoragePath)
+					?? panic("Could not borrow reference to sale V2 in storage")
+
+				// withdraw the moment from the sale, thereby de-listing it
+				let token <- topshotSaleCollection.withdraw(tokenID: tokenID)
+
+				// deposit the moment into the owner's collection	
+				nftCollection.deposit(token: <-token)
+
+				// List the specified moment for sale
+				topshotSaleV2Collection.listForSale(tokenID: tokenID, price: price)
+
+			}
+		}`
+
+	return []byte(fmt.Sprintf(template, topshotAddr, marketV1Addr, marketV2Addr))
+}
+
 // GenerateChangePercentageV2Script creates a cadence transaction that changes the cut percentage of an existing sale
 func GenerateChangePercentageV2Script(topshotAddr, marketAddr flow.Address, percentage float64) []byte {
 	template := `
