1d3e0455bbb3ed82355c6bcae5f843fb412afac9
Taylor Petrychyn <tpetrych@Taylors-MacBook-Pro.local>
2022-06-22 14:43:31 -0700

    add nft locking address template

--- lib/go/templates/template_utils.go
+++ lib/go/templates/template_utils.go
@@ -21,6 +21,7 @@ const (
 	placeholderDUCAddress             = "0xDUCADDRESS"
 	placeholderForwardingAddress      = "0xFORWARDINGADDRESS"
 	placeholderMetadataViewsAddress   = "0xMETADATAVIEWSADDRESS"
+	placeholderNFTLockingAddress 	  = "0xNFTLOCKINGADDRESS"
 )
 
 type Environment struct {
@@ -36,6 +37,7 @@ type Environment struct {
 	DUCAddress             string
 	ForwardingAddress      string
 	MetadataViewsAddress   string
+	NFTLockingAddress      string
 }
 
 func uint32ToCadenceArr(nums []uint32) []byte {
@@ -125,6 +127,12 @@ func replaceAddresses(code string, env Environment) string {
 		code,
 		placeholderMetadataViewsAddress,
 		withHexPrefix(env.MetadataViewsAddress),
+	)
+
+	code = strings.ReplaceAll(
+		code,
+		placeholderNFTLockingAddress,
+		withHexPrefix(env.NFTLockingAddress),
 	)
 
 	return code
